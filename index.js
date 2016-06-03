8200c6525c26d7cedcb84749b4da6a404e1a1e71
snupa <bunta.adrian@gmail.com>
2016-06-03 16:09:16 +0300

    added model extending functionality.

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ require('./lib/customTypes');
  * Events:
  *  - reconnect({name, duration})
  *  - disconnect({name})
+ *  NOTE:
+ *  - in order to extend a model added by a plugin, you can simply create the models/tableName.js file in your app, and
+ *  module.exports = function extend(modelObj, Seq) {}  // you MUST name your exported function "extend", in order to extend an object.
  */
 module.exports = function init(thorin, opt) {
   const async = thorin.util.async;
