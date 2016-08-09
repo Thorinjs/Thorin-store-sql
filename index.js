ff0b2246b164a76a3d930b0b2958a4a96a763a34
snupa <bunta.adrian@gmail.com>
2016-08-09 12:45:19 +0300

    added Seq.UUID as a custom type

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
-const storeInit = require('./lib/sqlStore');
-require('./lib/customTypes');
+const storeInit = require('./lib/sqlStore'),
+  typesInit = require('./lib/customTypes');
 /**
  * Created by Adrian on 29-Mar-16.
  * Events:
@@ -11,6 +11,7 @@ require('./lib/customTypes');
  *  module.exports = function extend(modelObj, Seq) {}  // you MUST name your exported function "extend", in order to extend an object.
  */
 module.exports = function init(thorin, opt) {
+  typesInit(thorin);
   const async = thorin.util.async;
   // Attach the SQL error parser to thorin.
   thorin.addErrorParser(require('./lib/errorParser'));
